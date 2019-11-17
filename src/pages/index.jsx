19e1a4da62bc6cca0ef8c825f59641fab3d350a0
saul <saul>
2019-11-17 15:45:36 +0200

    try repo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
+import { Foo } from "../components/layout1";;
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
